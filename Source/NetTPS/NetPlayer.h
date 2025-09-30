52a1f0232f1ecd66c987d6e76f3d626d6922144a
f11m4at4 <f11m4at4@gmail.com>
2025-09-30 09:58:41 +0900

    Initial commit

---
+++ Source/NetTPS/NetPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NetTPSCharacter.h"
+#include "NetPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETTPS_API ANetPlayer : public ANetTPSCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/NetTPS/NetPlayerAnim.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "NetPlayerAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETTPS_API UNetPlayerAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
