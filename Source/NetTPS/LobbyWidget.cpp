fddeb8fa5800d9a99b69eb69d3384363bb2223fd
lokimve7 <lokimve7@gmail.com>
2025-10-22 10:37:37 +0900

    251022 Lobby 화면 구성 02

--- Source/NetTPS/LobbyWidget.cpp
+++ Source/NetTPS/LobbyWidget.cpp
@@ -5,10 +5,17 @@
 
 #include "NetGameInstance.h"
 #include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Components/Slider.h"
+#include "Components/TextBlock.h"
+#include "Components/WidgetSwitcher.h"
 
 void ULobbyWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
+
+	// 마우스 커서 보이게
+	GetWorld()->GetFirstPlayerController()->SetShowMouseCursor(true);
 
 	// Game Instance 가져오자
 	gi = Cast<UNetGameInstance>(GetGameInstance());
@@ -16,12 +23,46 @@ void ULobbyWidget::NativeConstruct()
 	btnGoCreate->OnClicked.AddDynamic(this, &ULobbyWidget::OnClickGoCreate);
 	// 세션 조회 버튼 클릭 함수 등록
 	btnGoFind->OnClicked.AddDynamic(this, &ULobbyWidget::OnClickGoFind);
+
+	// 생성 버튼 클릭 함수 등록
+	btnCreate->OnClicked.AddDynamic(this, &ULobbyWidget::OnClickCreate);
+	// 세션 이름 변경 될 때 호출되는 함수 등록
+	editSessionName->OnTextChanged.AddDynamic(this, &ULobbyWidget::OnValueChangedSessionName);
+	// 인원 수 변경 될 때 호출되는 함수 등록
+	sliderPlayerCount->OnValueChanged.AddDynamic(this, &ULobbyWidget::OnValueChangedPlayerCount);
 }
 
 void ULobbyWidget::OnClickGoCreate()
 {
+	// 세션 생성 화면으로 이동
+	widgetSwitcher->SetActiveWidgetIndex(1);
 }
 
 void ULobbyWidget::OnClickGoFind()
 {
+	// 세션 조회 화면으로 이동
+	widgetSwitcher->SetActiveWidgetIndex(2);
+}
+
+void ULobbyWidget::OnClickCreate()
+{
+	// 세션 이름
+	FString sessionName = editSessionName->GetText().ToString();
+	// 인원 수
+	int32 playerCount = sliderPlayerCount->GetValue();
+	// 세션 생성
+	gi->CreateMySession(sessionName, playerCount);
+}
+
+void ULobbyWidget::OnValueChangedSessionName(const FText& text)
+{
+	// text 값의 길이에 따른 생성 버튼 활성 / 비활성
+	// text.IsEmpty()
+	btnCreate->SetIsEnabled(text.ToString().Len() > 0);
+}
+
+void ULobbyWidget::OnValueChangedPlayerCount(float value)
+{
+	// value 값을 textPlayerCount 에 설정
+	textPlayerCount->SetText(FText::AsNumber(value));
 }
--- Source/NetTPS/LobbyWidget.h
+++ Source/NetTPS/LobbyWidget.h
@@ -38,4 +38,28 @@ public:
 	// 세션 조회 화면 이동 함수
 	UFUNCTION()
 	void OnClickGoFind();
+
+	// 세션 생성 화면 관련
+	// 세션 이름 입력
+	UPROPERTY(meta=(BindWidget))
+	class UEditableTextBox* editSessionName;
+	// 인원수 Slider
+	UPROPERTY(meta=(BindWidget))
+	class USlider* sliderPlayerCount;
+	// 인원수 Text
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* textPlayerCount;
+	// 생성 버튼
+	UPROPERTY(meta=(BindWidget))
+	class UButton* btnCreate;
+	// 생성 버튼 눌렀을 때 호출되는 함수
+	UFUNCTION()
+	void OnClickCreate();
+	// 세션 이름 변경 될 때 호출되는 함수
+	UFUNCTION()
+	void OnValueChangedSessionName(const FText& text);
+	// 인원수 Slider 값이 변경 될 때 호출되는 함수
+	UFUNCTION()
+	void OnValueChangedPlayerCount(float value);
+	
 };
